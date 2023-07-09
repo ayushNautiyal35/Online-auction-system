1eb3f3559ced6990eccc491e4fc4a7e6c584d70f
Ayush Nautiyal <95178387+ayushNautiyal35@users.noreply.github.com>
2023-07-09 14:15:00 +0530

    Update show_content.php

--- show_content.php
+++ show_content.php
@@ -13,7 +13,9 @@ include("table_retrieve.php");
          <th>End DAte</th>
          <th>End_time</th>
          <th>base_value</th>
+         <th>Image</th>
          <th>item_info</th>
+         
 </tr>
     
   <?php
@@ -28,6 +30,8 @@ include("table_retrieve.php");
       
       <td><?php echo $data['BaseValue']??''; ?></td>
       <td><?php echo $data['Description']??''; ?></td>  
+      <?php $folder="uploads/".$data['Image']; ?>
+      <td><?php echo "<img src='$folder' height='50px'>"; ?></td>
       <td><?php echo '<a id="grey" href="intersted.php?id='.$data['ItemId'].'">'.'Interested'.'</a>'; ?> </td>
      </tr>
      <?php
